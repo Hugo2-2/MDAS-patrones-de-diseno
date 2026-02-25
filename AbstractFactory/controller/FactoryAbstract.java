be874f7b8254edd72889c49a439471a83f95da44
RafaLuque <rafaluquelaplana@gmail.com>
2026-02-25 12:15:16 +0100

    FactoryAbstract

---
+++ AbstractFactory/controller/FactoryAbstract.java
@@ -0,0 +1,8 @@
+package Bloque1.AbstractFactory.controller;
+
+import Bloque1.AbstractFactory.model.Menu;
+
+public abstract class FactoryAbstract {
+    public abstract Menu crearMenuSemanal();
+    public abstract Menu crearMenuTemporada();
+}
