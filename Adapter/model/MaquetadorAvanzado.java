29c9fd403a4c3c5e501004dfd61405abfb2160d1
joseluugc <jluisgc05@gmail.com>
2026-02-26 11:53:56 +0100

    addtext

--- Adapter/model/MaquetadorAvanzado.java
+++ Adapter/model/MaquetadorAvanzado.java
@@ -5,6 +5,6 @@ import java.util.Vector;
 
 public abstract class MaquetadorAvanzado {
     public abstract void joinFiles(File file1, File file2);
-    public abstract void combineParagraphs(File file1, Vector<Paragraph> vector1, File file2, Vector<Paragraph> vector2);
+    public abstract File combineParagraphs(File file1, Vector<Paragraph> vector1, File file2, Vector<Paragraph> vector2);
     public abstract Vector<File> splitFiles();
 }
--- Adapter/model/MaquetadorBasico.java
+++ Adapter/model/MaquetadorBasico.java
@@ -1,12 +1,25 @@
 package Adapter.model;
 
-import java.io.File;
+import java.io.*;
 import java.util.Vector;
 
 public class MaquetadorBasico {
     //Funcion para añadir texto
     void addText(File file, String text) {
-        //Implementar
+
+        //Usaremos un try with resources para evitar usar finally despues del catch
+        //asi acortaremos el código.
+
+        // El 'true' en el FileWriter indica que queremos añadir al final (append mode)
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+
+            out.println(text);
+
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
     }
 
     //Funcion para obtener un párrafo, pasando línea de inicio y final
