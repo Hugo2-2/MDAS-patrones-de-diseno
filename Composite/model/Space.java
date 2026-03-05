218130121515fc735b692b6a44286cae39e775dd
joseluugc <jluisgc05@gmail.com>
2026-03-05 11:22:42 +0100

    space.java hecho

--- Composite/model/Space.java
+++ Composite/model/Space.java
@@ -1,4 +1,20 @@
 package Composite.model;
 
+import java.util.List;
+
 public class Space extends Consumer {
+    private List<Consumer> children;
+
+    public Space(List<Consumer> children) {
+        this.children = children;
+    }
+
+    @Override
+    public double calcExpenses() {
+        double totalExpenses = 0;
+        for (Consumer child : children) {
+            totalExpenses += child.calcExpenses();
+        }
+        return totalExpenses;
+    }
 }
