449f4467d7e18c7b597ff409eb0981bac57a589c
RafaLuque <rafaluquelaplana@gmail.com>
2026-03-05 11:32:17 +0100

    Device implementacion

--- Composite/model/Device.java
+++ Composite/model/Device.java
@@ -1,4 +1,32 @@
 package Composite.model;
 
 public class Device extends Consumer {
+    private double consumptionPerHour;  // consumo por hora en kWh
+    private double estimatedHours;      // horas estimadas de uso
+
+    public Device(double consumptionPerHour, double estimatedHours) {
+        this.consumptionPerHour = consumptionPerHour;
+        this.estimatedHours = estimatedHours;
+    }
+
+    @Override
+    public double calcExpenses() {
+        return consumptionPerHour * estimatedHours;
+    }
+
+    public double getConsumptionPerHour() {
+        return consumptionPerHour;
+    }
+
+    public void setConsumptionPerHour(double consumptionPerHour) {
+        this.consumptionPerHour = consumptionPerHour;
+    }
+
+    public double getEstimatedHours() {
+        return estimatedHours;
+    }
+
+    public void setEstimatedHours(double estimatedHours) {
+        this.estimatedHours = estimatedHours;
+    }
 }
